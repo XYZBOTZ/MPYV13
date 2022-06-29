69fa0f59d70d9b7c700ea33bf53e672d5d5d460e
XYZ3 <107297177+XYZBOTZ@users.noreply.github.com>
2022-06-29 17:53:29 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,21 +18,21 @@ global.autoread = true // auto read pesan / message
 //global.available = false //status online (online)
 
 // Other
-global.botname = "PENTABOT-MD" //namabot kalian
-global.ownername= "Gempy×፝֟͜×" //nama kalian
-global.myweb ="https://www.youtube.com/channel/UCZoVmApPxtLYgUWwBD4nbCw" //bebas asal jan hapus
-global.youtube = "https://www.youtube.com/channel/UCZoVmApPxtLYgUWwBD4nbCw" //bebas asal jan hapus
-global.github = "https://github.com/GempyTon" //bebas
+global.botname = "XYZ-MD" //namabot kalian
+global.ownername= "Nopan×፝֟͜×" //nama kalian
+global.myweb ="Apa lu anj" //bebas asal jan hapus
+global.youtube = "GA PUNYA ANJ" //bebas asal jan hapus
+global.github = "https://github.com/XYZBOTZ" //bebas
 global.email = "gempyhaxor@hotmail.com" //bebas
 global.region = "Indonesia" //bebas
-global.ownernomer = "6287898307350" // nomor wa kalian
-global.ownernomerr = "+6287898307350" //nmr wa kalian
+global.ownernomer = "6285862754807" // nomor wa kalian
+global.ownernomerr = "+6285862754807" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://i.postimg.cc/L84bLpxL/menu-1.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.owner = ["6287898307350","6287898307350"] //ganti agar fitur owner bisa di gunakan
-global.packname = '©PENTABOTོ' //sticker wm ubah
-global.author = 'Gempy' //sticker wm ganti nama kalian
+global.packname = '© Xyz-Md' //sticker wm ubah
+global.author = 'Nopan' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
